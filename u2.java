9f3c8acae5ff7965f2fccd47918d6de1be84f85a
likhithashylu <likitha.shylu@gmail.com>
2024-09-10 23:35:15 +0530

    checking dailywages

---
+++ u2.java
@@ -0,0 +1,14 @@
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+        // Constants
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+
+        // Calculate daily wage
+        int dailyWage = wagePerHour * fullDayHour;
+
+        // Display the daily wage
+        System.out.println("Employee Daily Wage is: $" + dailyWage);
+    }
+}
